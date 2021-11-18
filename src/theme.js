74630f3d8027e3851108c86ca76138ee5a2e5281
Doni-zete <donizete112@gmail.com>
2021-11-17 22:04:45 -0300

    adicionando arquivos

---
+++ src/theme.js
@@ -0,0 +1,13 @@
+const theme = {
+  colors: {
+    primary: '#6200ee',
+    background: '#00000014',
+    text: '#00000000B3',
+    sucess: '#135E01',
+  },
+  fonts: {
+  regular: 'Roboto,san-serif',
+},
+};
+
+export default theme;
