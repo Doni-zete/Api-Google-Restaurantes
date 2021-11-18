055f45c4cd7ba602cd96ee615430317ede1a4fa2
Doni-zete <donizete112@gmail.com>
2021-11-18 20:09:25 -0300

    adiconando funcinalidades

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,3 +1,26 @@
-import React from 'react';
-const Home =()=><div>Hello world!</div>;
+import React, { useState } from 'react';
+import logo from '../../assets/logo.svg';
+import TextField, { Input } from '@material/react-text-field';
+import { Container, Search } from './style';
+
+const Home = () => {
+  const [inputValue, setInputValue] = useState('cachorro');
+  return (
+
+    <Container>
+      <Search>
+        <img src={logo} alt="Logo do restaurante" />
+        <TextField
+          label='Pesquisar'
+          //helperText={<HelperText>Help Me!</HelperText>}
+          //onTrailingIconSelect={() => this.setState({ value: '' })}
+        //trailingIcon={<MaterialIcon role="button" icon="delete"/>}
+        ><Input
+            value={inputValue}
+            onChange={(e) => setInputValue(e.target.value)}/>
+        </TextField>
+      </Search>
+    </Container>
+  );
+};
 export default Home;
--- src/pages/Home/style.js
+++ src/pages/Home/style.js
@@ -1,2 +1,15 @@
-import styled  from "styled-components";
-const Container = styled.div
+import styled from "styled-components";
+export const Container = styled.aside`
+background-color:${(props) => props.theme.colors.background};
+width:360px;
+height: 100vh;
+overflow-y:auto;
+`;
+
+export const Search = styled.section`
+display: flex;
+flex-direction: column;
+justify-center:center;
+background-color: #ffffff;
+padding:16px;
+`
